c8e09804a62aa6039e3eafe437e4573090d3dffc
Adam Scanga <sandmanscanga@gmail.com>
2020-12-16 17:20:25 -0600

    Set project folder path to be indiscriminate

--- project/consts.py
+++ project/consts.py
@@ -1,5 +1,8 @@
 '''Module to store project constants'''
+import os
+
 
 LOG_LEVEL = 10  # logging.DEBUG
-CACHE_BASE = "cache"
+PROJECT_PATH = os.path.sep.join(__file__.split(os.path.sep)[:-1])
+CACHE_BASE = f"{PROJECT_PATH}/cache"
 BASE_URL = "https://www.espn.com"
